4fd8493883136cc4cfb7c08eb4b85270f4ae0592
Dominik <d.rolewski98@gmail.com>
2021-02-09 20:41:15 +0100

    My project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './sass/main.scss'
+import {useEffect, useState} from "react";
+import {addNewNote, getAllNotes} from "./services/api-service";
 
 
-function App() {
+function App(){
+  const [notes, setNotes] = useState([]);
+  useEffect(  () =>{
+    getAllNotes(data => setNotes(data), err => console.log());
+  },[])
+
+  const handleAddNote = () => {
+    //create noteObje
+    const note = {
+      content: "",
+
+    }
+    addNewNote(note);
+  }
   return (
     <div className="App">
 
@@ -10,3 +25,34 @@ function App() {
 }
 
 export default App;
+//app jest odpalony w pliku index.js
+//a w tym pliku muszę z niego skorzystać :)
+//dlatego jest importowany
+
+
+//DAREK:
+// import './sass/main.scss'
+// import {useEffect, useState} from "react";
+// import {addNewNote, getAllNotes} from "./services/api-service";
+//
+//
+// function App(){
+//   const [notes, setNotes] = useState([]);
+//   useEffect(  () =>{
+//     getAllNotes(data => setNotes(data), err => console.log());
+//   },[])
+//
+//   const handleAddNote = () => {
+//     //create noteObje
+//     const note = {
+//       content: "",
+//
+//     }
+//     addNewNote(note);
+//   }
+//   return (
+//       <div className="App">
+//
+//       </div>
+//   );
+// }
---
+++ src/services/api-service.js
@@ -0,0 +1,24 @@
+
+const url = 'http://localhost:3000';
+
+const getAllNotes = ( callback, errorCallback ) =>{
+    fetch(`${url}/notes`)
+        .then(resp => resp.json())
+        .then(data => callback(data) )
+        .catch(e => errorCallback(e));
+}
+
+const addNewNote = (note, callback, errorCallback) => {
+    fetch(`${url}/notes`,{
+        method:"POST",
+        body: JSON.parse(note),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    })
+        .then(resp => resp.json())
+        .then(data => callback(data) )
+        .catch(e => errorCallback(e));
+}
+
+export {getAllNotes, addNewNote}
